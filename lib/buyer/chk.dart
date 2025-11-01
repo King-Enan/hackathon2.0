cdd2799560c79d90ecf5cf46491c4bd50dde7146
JoyyY1209 <subahojoy12@gmail.com>
2025-11-01 23:15:10 +0600

    2nd

---
+++ lib/buyer/chk.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+class chk extends StatefulWidget {
+  const chk({super.key});
+
+  @override
+  State<chk> createState() => _chkState();
+}
+
+class _chkState extends State<chk> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("chkj"),
+      ),
+    );
+  }
+}
